9f5458fc3db2224680ffcdcd1ba1b165d7aee3c1
dschmaryl <dschmaryl@gmail.com>
2018-10-29 13:22:55 -0400

    fix tick volume

--- App/screens/Main/components/Tick.js
+++ App/screens/Main/components/Tick.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Sound from 'react-native-sound';
 
 export class Tick extends React.Component {
-  state = { volume: 50, seconds: null };
+  state = { tickVolume: null, seconds: null };
 
   tick = new Sound('tick.mp3', null, error => {
     if (error) {
@@ -11,13 +11,23 @@ export class Tick extends React.Component {
     }
   });
 
-  componentWillReceiveProps = newProps => {
-    if (newProps.volume !== this.state.volume)
-      this.setState({ volume: newProps.volume }, () =>
-        this.tick.setVolume(newProps.tickVolume / 100)
-      );
+  componentDidMount = () =>
+    this.setState(
+      { tickVolume: this.props.tickVolume, seconds: this.props.seconds },
+      () => this.tick.setVolume(this.props.tickVolume / 100)
+    );
 
-    if (newProps.seconds !== this.state.seconds && newProps.tickIsEnabled) {
+  componentWillReceiveProps = newProps => {
+    if (newProps.tickVolume !== this.state.tickVolume) {
+      this.tick.setVolume(newProps.tickVolume / 100);
+      this.setState({ tickVolume: newProps.tickVolume });
+    }
+
+    if (
+      newProps.seconds !== this.state.seconds &&
+      !newProps.isPaused &&
+      newProps.tickIsEnabled
+    ) {
       this.tick.play();
       this.setState({ seconds: newProps.seconds });
     }
--- App/screens/Main/containers/Tick.js
+++ App/screens/Main/containers/Tick.js
@@ -3,8 +3,10 @@ import { connect } from 'react-redux';
 import { Tick } from '../components/Tick';
 
 const mapStateToProps = state => ({
+  isPaused: state.timer.isPaused,
   seconds: state.timer.seconds,
-  tickIsEnabled: state.settings.tickIsEnabled
+  tickIsEnabled: state.settings.tickIsEnabled,
+  tickVolume: state.settings.tickVolume
 });
 
 export default connect(mapStateToProps)(Tick);
