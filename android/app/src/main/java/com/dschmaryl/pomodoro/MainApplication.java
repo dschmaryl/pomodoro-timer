6c92071d49aa4dfa0d16689471011377a6060df7
dschmaryl <dschmaryl@gmail.com>
2019-04-18 20:41:03 -0400

    switch to react-native-video for sound

--- android/app/src/main/java/com/dschmaryl/pomodoro/MainApplication.java
+++ android/app/src/main/java/com/dschmaryl/pomodoro/MainApplication.java
@@ -3,6 +3,7 @@ package com.dschmaryl.pomodoro;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.wheelpicker.WheelPickerPackage;
 import com.corbt.keepawake.KCKeepAwakePackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new WheelPickerPackage(),
             new KCKeepAwakePackage(),
             new RNSoundPackage(),
