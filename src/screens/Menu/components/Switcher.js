e60a31eddf6c5683de4d19db8661e2ce3d6fa027
dschmaryl <dschmaryl@gmail.com>
2019-06-01 18:24:35 -0400

    disable alarm stuff when alarm is disabled

--- src/screens/Menu/components/Switcher.js
+++ src/screens/Menu/components/Switcher.js
@@ -4,21 +4,29 @@ import Switch from 'react-native-switch-pro';
 
 import { styles } from './styles';
 
-export const Switcher = ({ label, isOn, colors, onToggle }) => (
-  <TouchableNativeFeedback onPress={onToggle}>
-    <View style={[styles.rowView, colors.borderColor]}>
-      <Text style={[styles.rowText, colors.textColor]}>{label}:</Text>
-      <View style={styles.switchView}>
-        <Switch
-          onSyncPress={onToggle}
-          value={isOn}
-          style={styles.switch}
-          circleColorActive={colors.backgroundColor.backgroundColor}
-          circleColorInactive={colors.backgroundColor.backgroundColor}
-          backgroundActive={colors.buttonColor.color}
-          backgroundInactive={colors.borderColor.borderColor}
-        />
+export const Switcher = ({ label, isOn, onToggle, disabled, colors }) => {
+  const textColor = disabled ? colors.disabledColor : colors.textColor;
+  const buttonColor = disabled
+    ? colors.disabledColor.color
+    : colors.buttonColor.color;
+
+  return (
+    <TouchableNativeFeedback disabled={disabled} onPress={onToggle}>
+      <View style={[styles.rowView, colors.borderColor]}>
+        <Text style={[styles.rowText, textColor]}>{label}:</Text>
+        <View style={styles.switchView}>
+          <Switch
+            onSyncPress={onToggle}
+            value={isOn}
+            style={styles.switch}
+            circleColorActive={colors.backgroundColor.backgroundColor}
+            circleColorInactive={colors.backgroundColor.backgroundColor}
+            backgroundActive={buttonColor}
+            backgroundInactive={colors.borderColor.borderColor}
+            disabled={disabled}
+          />
+        </View>
       </View>
-    </View>
-  </TouchableNativeFeedback>
-);
+    </TouchableNativeFeedback>
+  );
+};
