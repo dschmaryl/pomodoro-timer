3f6aeb796bcdc3f5df158d0c90bdff4bec5ec63e
dschmaryl <dschmaryl@gmail.com>
2019-08-09 11:52:41 -0400

    adjust tick component

--- src/screens/Main/components/Tick.js
+++ src/screens/Main/components/Tick.js
@@ -4,51 +4,45 @@ import Video from 'react-native-video';
 import { tickSounds } from '../../../sounds';
 
 export class Tick extends React.PureComponent {
-  state = { muteTicks: true, appState: 'active', seconds: 0 };
+  state = {
+    muteTicks: true,
+    tickSoundIndex: 0,
+    tickVolume: 50,
+    appState: 'active',
+    seconds: 0
+  };
 
   static getDerivedStateFromProps = (nextProps, prevState) => {
     if (nextProps.playTicks) {
       if (nextProps.appState === prevState.appState) {
         if (nextProps.seconds !== prevState.seconds) {
           if (prevState.muteTicks) {
-            return {
-              ...prevState,
-              muteTicks: false,
-              seconds: nextProps.seconds
-            };
+            return { ...nextProps, muteTicks: false };
           } else {
-            return { ...prevState, seconds: nextProps.seconds };
+            return { ...nextProps };
           }
         }
       } else {
         if (nextProps.appState === 'active') {
-          return {
-            muteTicks: true,
-            appState: 'active',
-            seconds: nextProps.seconds
-          };
+          return { ...nextProps, muteTicks: true };
         } else {
-          return {
-            muteTicks: false,
-            appState: nextProps.appState,
-            seconds: nextProps.seconds
-          };
+          return { ...nextProps, muteTicks: false };
         }
       }
     } else {
       if (!prevState.muteTicks) {
-        return { ...prevState, muteTicks: true };
+        return { ...nextProps, muteTicks: true };
       }
     }
     return null;
   };
 
   render = () =>
-    !this.props.playTicks ? null : (
+    this.state.muteTicks ? null : (
       <Video
         key={this.state.seconds}
-        source={tickSounds[this.props.tickSoundIndex].soundFile}
-        volume={this.state.muteTicks ? 0 : this.props.tickVolume / 100}
+        source={tickSounds[this.state.tickSoundIndex].soundFile}
+        volume={this.state.muteTicks ? 0 : this.state.tickVolume / 100}
       />
     );
 }
